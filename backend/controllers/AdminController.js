1f2a3ab6e5b1ab9f897ef1c8eba44a51b26d8f34
kalyanasundar8 <sundar31100@gmail.com>
2024-06-11 19:51:16 +0530

    Admin & User authentication completed commit

--- backend/controllers/AdminController.js
+++ backend/controllers/AdminController.js
@@ -2,6 +2,7 @@ import asyncHandler from "express-async-handler";
 import Admin from "../models/AdminModel.js";
 import { generateOTP } from "../services/OtpService.js";
 import generateToken from "../services/TokenService.js";
+import jwt from "jsonwebtoken";
 
 //! Signup
 const adminSignup = asyncHandler(async (req, res) => {
@@ -45,4 +46,92 @@ const adminSignup = asyncHandler(async (req, res) => {
   }
 });
 
-export { adminSignup };
+//! Verify Otp
+const adminVerifyOtp = asyncHandler(async (req, res) => {
+  const { otp, token } = req.body;
+
+  const decoded = jwt.verify(token, process.env.SECRET_KEY);
+  const adminId = decoded.id;
+
+  const userExists = await Admin.findOne({ _id: adminId });
+
+  if (userExists) {
+    if (userExists.otp === otp) {
+      await Admin.findOneAndUpdate(
+        { _id: adminId },
+        {
+          verified: true,
+        }
+      );
+      res.status(200).json({ mssg: "Verified sucessfuly" });
+    } else if (userExists.signinOtp === otp) {
+      res.status(200).json({
+        id: userExists._id,
+        companyName: userExists.companyName,
+        adminName: userExists.adminName,
+        companyType: userExists.companyType,
+        mobileNumber: userExists.mobileNumber,
+        verified: userExists.verified,
+        token: generateToken(userExists._id),
+      });
+    } else if (userExists.otp !== otp) {
+      res.status(400).json({ err: "Invalid OTP" });
+    }
+  }
+});
+
+//! SignIn
+const adminSignin = asyncHandler(async (req, res) => {
+  const { mobileNumber } = req.body;
+
+  // Check mobile number format
+  const mobFormat = /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$/;
+  const validMobNumber = mobFormat.test(mobileNumber);
+
+  if (!validMobNumber) {
+    res.status(400).json({ mssg: "Not a valid mobile number" });
+  }
+
+  // Check mobile number exists
+  const numberExists = await Admin.findOne({ mobileNumber });
+
+  if (numberExists === null) {
+    res
+      .status(400)
+      .json({ mssg: "Mobilenumber not exists please create an account " });
+  } else if (numberExists.verified === false) {
+    const otp = generateOTP();
+    const admin = await Admin.findOneAndUpdate(
+      { _id: numberExists.id },
+      { $set: { otp: otp } },
+      { new: true }
+    );
+    res.status(200).json({
+      id: admin._id,
+      companyName: admin.companyName,
+      adminName: admin.adminName,
+      companyType: admin.companyType,
+      mobileNumber: admin.mobileNumber,
+      token: generateToken(user._id),
+      mssg: "We sent a OTP to you mobileNumber, Just verify and continue this account",
+    });
+  } else {
+    const otp = generateOTP();
+    const admin = await Admin.findOneAndUpdate(
+      { _id: numberExists.id },
+      { $set: { signinOtp: otp } },
+      { new: true }
+    );
+    res.status(200).json({
+      id: admin._id,
+      companyName: admin.companyName,
+      adminName: admin.adminName,
+      companyType: admin.companyType,
+      mobileNumber: admin.mobileNumber,
+      token: generateToken(admin._id),
+      mssg: "We sent a OTP to you mobileNumber",
+    });
+  }
+});
+
+export { adminSignup, adminVerifyOtp, adminSignin };
--- backend/routes/AdminRoutes.js
+++ backend/routes/AdminRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { adminSignup } from "../controllers/AdminController.js";
+import { adminSignin, adminSignup, adminVerifyOtp } from "../controllers/AdminController.js";
 
 const adminRoutes = express.Router();
 
 adminRoutes.post("/", adminSignup);
+adminRoutes.post("/verifyotp", adminVerifyOtp);
+adminRoutes.post("/adminsignin", adminSignin);
 
 export default adminRoutes;
