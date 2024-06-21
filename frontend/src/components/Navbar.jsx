62961517981974882b7721825401c7aaa3a0f077
kalyanasundar8 <sundar31100@gmail.com>
2024-06-21 15:23:20 +0530

    New commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,10 +8,8 @@ const Navbar = () => {
     <div className="w-full bg-white shadow-sm py-3 px-[20px] flex items-center justify-between md:px-[100px] xl:py-[0px]">
       <h1 className="font-logoFont text-xl md:text-2xl xl:text-2xl">LBR</h1>
       <nav className="font-primaryFont text-[15px] md:text-[18px] md:py-4 flex items-center space-x-5">
-        {user.verified === true ? (
-          <h1>{user.PassengerName}</h1>
-        ) : (
-          <div>
+        {user === null ? (
+          <div className="flex items-center space-x-2">
             <Link className="">Sign In</Link>
             <Link
               to="/signup"
@@ -20,6 +18,10 @@ const Navbar = () => {
               Sign Up
             </Link>
           </div>
+        ) : user.verified === true ? (
+          <h1>{user.PassengerName}</h1>
+        ) : (
+          ""
         )}
       </nav>
     </div>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="mx-[30px] mt-3 xl:mx-[150px] xl:mt-[50px] font-primaryFont">
+      <form className="flex flex-col space-y-3 xl:flex xl:flex-row xl:items-center xl:justify-between">
+        <select className="w-[330px] xl:w-[300px] border-2 border-gray-300 px-[10px] py-[4px] rounded-[3px]">
+          <option disabled>Location</option>
+          <option>Tenkasi</option>
+          <option>Surandai</option>
+          <option>Tirunelveli</option>
+        </select>
+        <div className="flex items-center space-x-2">
+          <select className="border-2 border-gray-300 w-[200px] px-[10px] py-[4px] rounded-[3px]">
+            <option disabled>From</option>
+            <option>Tenkasi</option>
+            <option>Surandai</option>
+            <option>Tirunelveli</option>
+          </select>
+          <select className="border-2 border-gray-300 w-[200px] px-[10px] py-[4px] rounded-[3px]">
+            <option disabled>To</option>
+            <option>Tenkasi</option>
+            <option>Surandai</option>
+            <option>Tirunelveli</option>
+          </select>
+        </div>
+      </form>
+      <section className="flex items-baseline justify-between mt-10">
+        <div className="flex items-center justify-between w-[550px]">
+          <div>
+            <h1 className="font-bold text-[23px]">Ganapathy</h1>
+            <p className="text-[15px]">Surandai to Puliyankudi</p>
+            <p className="text-[15px]">2.00 PM</p>
+          </div>
+          <div className="bg-red-500 px-4 py-3 font-bold rounded-full">G</div>
+        </div>
+        <div className="border-2 border-gray-300 w-[600px] h-[400px] hidden xl:block">
+            <p>Click bus and find routes</p>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
